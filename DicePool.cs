463b60ca3ca62b9dc4313abf575008541c9f7e9f
Levariel <levarielravagraiddemorial@gmail.com>
2022-09-15 01:07:59 +0100

    Fixed wierd issue with Exploding Hit Results. Added Table extension to use later.

--- DicePool.cs
+++ DicePool.cs
@@ -1,42 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DiceRollPrototype {
+﻿namespace DiceRollPrototype {
     internal class DicePool : Dice {
-        public Dictionary<int, int> Results;
+        public Dictionary<int,int> Results;
+        public Dictionary<int,int> ExResults;
         public int Blanks => Results[1];
         public int Counters => Results[2];
         public int HeavyCounters => Results[3];
-        public int SingleHits => Results[4];
-        public int HeavyHits => Results[5];
-        public int ExplodingHits => Results[6];
+        public int SingleHits => Results[4] + ExResults[4];
+        public int HeavyHits => Results[5] + ExResults[5];
+        public int ExplodingHits => Results[6] + ExResults[6];
         public int TotalHits => SingleHits + (HeavyHits * 2) + (ExplodingHits * 2);
 
         public DicePool() {
             //Initialise and populate the directory
-            Results = new Dictionary<int, int>();
-            for (int i = 1; i <= 6; i++) Results.Add(i, 0);
+            Results = new Dictionary<int,int>();
+            ExResults = new Dictionary<int,int>();
+            for (int i = 1; i <= 6; i++) {
+                Results.Add(i,0);
+                ExResults.Add(i,0);
+            }
         }
 
         //Roll the number of dice a number of times, adding each result to Dictionary
-        public void RollPool(int NumDice, int NumRoll) {
-            for (int i = 0; i < NumDice; i++)
-                for (int j = 0; j < NumRoll; j++)
+        public void RollPool(int NumDice,int NumRoll) {
+            for (int i = 0; i < NumDice; i++) {
+                for (int j = 0; j < NumRoll; j++) {
                     Results[Roll()]++;
+                }
+            }
             ExDiceRoll(Results[6]);
         }
 
         //Attempt at Exploding Dice. Probably wrong.
-        //Copy of dice rolling function without extra for loop.x
+        //Copy of dice rolling function without extra for loop.
         public void ExDiceRoll(int NumDice) {
-            if (NumDice < 1) { return; } else {
+            if (NumDice == 0) { return; } else {
                 for (int i = 0; i < NumDice; i++) {
-                    Results[Roll()]++;
+                    int temp = Roll();
+                    Results[temp]++;
+                    ExResults[temp]++;
                 }
-
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,36 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 DiceRollPrototype.DicePool d = new();
+
 Console.WriteLine("Please enter the number of dice:");
 int dicenum = Convert.ToInt16(Console.ReadLine());
-d.RollPool(dicenum, 1);
+d.RollPool(dicenum,1);
 PrintResults();
-//Console.WriteLine("");
-//Console.WriteLine($"Single Hits: {dicepool.SingleHits}");
-//Console.WriteLine($"Heavy Hits: {dicepool.HeavyHits}");
-//Console.WriteLine($"Exploding Hits: {dicepool.ExplodingHits}");
-//Console.WriteLine($"Total Hits: {dicepool.TotalHits}");
 
 void PrintResults() {
-    string[] ResultName = new string[] {"Blanks", "Counters", "Heavy Counters", 
+    string[] ResultName = new string[] {"Blanks", "Counters", "Heavy Counters",
                                         "Hits", "Heavy Hits", "Exploding Hits" };
+    Console.Write("\n\n");
+    //Loop through Dictionary to display results
+    Console.WriteLine("Results from dice pool");
     foreach (var item in d.Results) {
-        Console.WriteLine($"{ResultName[(int)item.Key - 1] + ":",-18}{item.Value}");
-        if (item.Key == 6) { 
-            Console.WriteLine($"\nTotal Hits: {d.TotalHits, 7}");
+        Console.WriteLine($"{ResultName[item.Key - 1] + ":",-18}{item.Value}");
+        //Loop through Exploding Dictionary for Ex Dice results
+        if (item.Key == 6) {
+            Console.WriteLine("\nResults from Exploding Hits.");
+            for (int i = 0; i < 3; i++) {
+                Console.WriteLine($"{ResultName[i + 3] + ":",-18}{d.ExResults[i + 4]}");
+            }
+            //Display total hits
+            Console.WriteLine($"\nTotal Hits: {d.TotalHits,7}");
         }
     }
 }
-//Console.WriteLine("Enter the number of dice:");
-//int num = Convert.ToInt16(Console.ReadLine());
-//Console.WriteLine($"Normal Hits:    {Indent(11)}{d.Roll(num,1)}");
-//Console.WriteLine($"With Sustained Or Homing: {Indent(1)}{d.Roll(num,2)}");
-//Console.WriteLine($"With Fusillade: {Indent(11)}{d.Roll(num,3)}");
-//Console.WriteLine($"Devastating: {Indent(14)}{d.Roll(num, 4)}");
-//Console.WriteLine("Press any key to close the program.");
 //Notes: Need to account for the following scenarios
-//NORMAL: 10.005729  - Done
-//SUSTAINED / HOMING: 11.663932 - Done
-//FUSILLADE / SUSTAINED AND HOMING: 13.333718 - Done
+//NORMAL: 10.005729 
+//SUSTAINED / HOMING: 11.663932 
+//FUSILLADE / SUSTAINED AND HOMING: 13.333718 
 //DEVASTATING: 11.999949
 //NORMAL_VS_OBSCURED: 8.33679
 //FUSILLADE / SUSTAINED AND HOMING_VS_OBSCURED: 11.112327
